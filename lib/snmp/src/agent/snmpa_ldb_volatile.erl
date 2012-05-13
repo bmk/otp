385252aab906841d221ed3473c119b73173ca3f6
Micael Karlberg <micael@mathros.(none)>
2012-05-13 21:36:36 +0200

    Added table functions

--- lib/snmp/src/agent/snmpa_ldb_volatile.erl
+++ lib/snmp/src/agent/snmpa_ldb_volatile.erl
@@ -24,8 +24,26 @@
 	 start_link/1,
 	 stop/0, 
 	 verbosity/1,
+
 	 variable_get/1, 
-	 variable_set/2
+	 variable_set/2, 
+	 variable_delete/1, 
+	 variable_inc/2, 
+
+	 table_create/1, 
+	 table_exists/1, 
+	 table_delete/1,
+         table_get/1,
+
+	 table_create_row/3, table_create_row/4, 
+         table_delete_row/2,
+         table_get_row/2,
+         table_get_element/3, 
+	 table_get_elements/4,
+         table_set_elements/3, 
+	 table_set_status/7,
+         table_next/2,
+         table_max_col/2
 	]).
 
 %% snmpa_ldb callback functions
@@ -65,6 +83,54 @@ variable_inc(Variable, N) ->
     snmpa_ldb:variable_inc(?NAME, Variable, N).
 
 
+table_create(Table) ->
+    snmpa_ldb:table_create(?NAME, Table).
+
+table_exists(Table) ->
+    snmpa_ldb:table_exists(?NAME, Table).
+
+table_delete(Table) ->
+    snmpa_ldb:table_delete(?NAME, Table).
+
+table_create_row(Table, RowIndex, Row) ->
+    snmpa_ldb:table_create_row(?NAME, Table, RowIndex, Row).
+
+table_create_row(Table, RowIndex, Status, Cols) ->
+    snmpa_ldb:table_create_row(?NAME, Table, RowIndex, Status, Cols).
+
+table_delete_row(Table, RowIndex) ->
+    snmpa_ldb:table_create_row(?NAME, Table, RowIndex).
+
+table_get_row(Table, RowIndex) ->
+    snmpa_ldb:table_get_row(?NAME, Table, RowIndex).
+
+table_get_element(Table, RowIndex, Col) ->
+    snmpa_ldb:table_get_element(?NAME, Table, RowIndex, Col).
+
+table_get_elements(Table, RowIndex, Cols, FOI) ->
+    snmpa_ldb:table_get_elements(?NAME, Table, RowIndex, Cols, FOI).
+
+table_set_elements(Table, RowIndex, Cols, FOI) ->
+    snmpa_ldb:table_get_elements(?NAME, Table, RowIndex, Cols, FOI).
+
+table_set_elements(Name, RowIndex, Cols) ->
+    snmpa_ldb:table_set_elements(?NAME, Table, RowIndex, Cols).
+
+table_set_status(Table, RowIndex, Status, StatusCol, Cols, 
+		 ChangedStatusFunc, ConsFunc) ->
+    snmpa_ldb:table_set_status(?NAME, Table, RowIndex, Status, StatusCol, Cols, 
+			       ChangedStatusFunc, ConsFunc).
+
+table_next(Table, RestOid) ->
+    snmpa_ldb:table_next(?NAME, Table, RestOid).
+
+table_get(Table) ->
+    snmpa_ldb:table(?NAME, Table).
+
+table_max_col(Table, Col) ->
+    snmpa_ldb:table_max_col(?NAME, Table, Col).
+
+
 %% -------------------------------------------------------------------- 
 %% snmpa_ldb callback functions
 
@@ -76,8 +142,8 @@ handle_close(#state{tab = Tab}) ->
     ets:delete(Tb),
     ok.
 
-handle_insert(#state{tab = Tab}, Key, Value) ->
-    ets:insert(Tab, {Key, Value}),
+handle_insert(#state{tab = Tab}, Data) ->
+    ets:insert(Tab, Data),
     ok.
 
 handle_delete(#state{tab = Tab}, Key) ->
